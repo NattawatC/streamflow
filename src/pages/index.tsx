609fa5d7bcbe3574595376e52827cd44fcadaeb7
NattawatC <64011478@kmitl.ac.th>
2024-10-07 22:19:39 +0700

    Initializing the project

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div>
+      <div className="font-satoshi">
+        <h1 className="font-medium text-3xl">Welcome to the Homepage</h1>
+        <p>This is a paragraph using the Satoshi font.</p>
+      </div>
+    </div>
+  )
+}
