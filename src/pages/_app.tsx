eb4f7d308816c625b4eb8bb5daa8fd3bda0766e8
NattawatC <64011478@kmitl.ac.th>
2024-10-08 17:57:06 +0700

    create const Subjectivity

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,28 +5,39 @@ import localFont from "next/font/local"
 const satoshi = localFont({
   src: [
     {
-      path: '../../public/fonts/Satoshi-Regular.otf',
-      weight: '400',
-      style: 'normal',
+      path: "../../public/fonts/Satoshi-Regular.otf",
+      weight: "400",
+      style: "normal",
     },
     {
-      path: '../../public/fonts/Satoshi-Medium.otf',
-      weight: '500',
-      style: 'Medium',
+      path: "../../public/fonts/Satoshi-Medium.otf",
+      weight: "500",
+      style: "Medium",
     },
     {
-      path: '../../public/fonts/Satoshi-Bold.otf',
-      weight: '700',
-      style: 'Bold',
-    }
+      path: "../../public/fonts/Satoshi-Bold.otf",
+      weight: "700",
+      style: "Bold",
+    },
   ],
-  variable: "--font-satoshi"
+  variable: "--font-satoshi",
+})
+
+const subjectivity = localFont({
+  src: [
+    {
+      path: "../../public/fonts/Subjectivity-Medium.otf",
+      weight: "500",
+      style: "Medium",
+    },
+  ],
+  variable: "--font-subjectivity",
 })
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <main className={`${satoshi.variable} font-satoshi`}>
+      <main className={`${satoshi.variable} font-satoshi ${subjectivity.variable}`}>
         <Component {...pageProps} />
       </main>
     </>
