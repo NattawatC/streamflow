b1aa08cde58e9345f8608c9dddab3277d9bfafcf
NattawatC <64011478@kmitl.ac.th>
2025-01-30 17:54:15 +0700

    Add information dialog on both page

--- src/pages/electricity.tsx
+++ src/pages/electricity.tsx
@@ -3,14 +3,25 @@ import { NextPage } from "next"
 import UtilityInfo from "@/components/utilityInfo"
 import { Button } from "@/components/ui/button"
 import { IoIosArrowDown } from "react-icons/io"
-import { IoInformationCircleOutline } from "react-icons/io5"
+import { IoInformationCircle } from "react-icons/io5"
+import { GoDotFill } from "react-icons/go"
 import { IoIosArrowBack } from "react-icons/io"
 import { MainLayout } from "@/components/layout"
 import Link from "next/link"
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
 
 const mockData = {
-  startDate: "2021-10-01",
-  endDate: "2021-10-30",
+  startDate: "2021/10/01",
+  endDate: "2021/10/30",
   used: 100,
   cost: 1000,
   total: 10000,
@@ -25,7 +36,35 @@ const electricity: NextPage = () => {
         </Link>
         <div className="flex flex-row items-center gap-2">
           <h1 className="font-bold text-2xl">Electricity Information</h1>
-          <IoInformationCircleOutline size={24} className="text-black" />
+          <Dialog>
+            <DialogTrigger>
+              <IoInformationCircle size={24} />
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader className="flex flex-col gap-4">
+                <DialogTitle className="flex text-left text-white">
+                  Electricity Information
+                </DialogTitle>
+                <DialogDescription className="flex text-white justify-start flex-col text-left gap-2">
+                  <div className="flex flex-row gap-2">
+                    <GoDotFill size={14} className="mt-1" />
+                    <p>
+                      This page shows the electricity usage information in this
+                      month.
+                    </p>
+                  </div>
+                  <div className="flex flex-row gap-2">
+                    <GoDotFill size={14} className="mt-1" />
+                    <p>
+                      Clicking on the{" "}
+                      <span className="text-custom-pink">"Electric Meter (Image)" </span>
+                      button to see the meter image.
+                    </p>
+                  </div>
+                </DialogDescription>
+              </DialogHeader>
+            </DialogContent>
+          </Dialog>
         </div>
         <UtilityInfo
           startDate={mockData.startDate}
--- src/pages/water.tsx
+++ src/pages/water.tsx
@@ -3,10 +3,21 @@ import { NextPage } from "next"
 import UtilityInfo from "@/components/utilityInfo"
 import { Button } from "@/components/ui/button"
 import { IoIosArrowDown } from "react-icons/io"
-import { IoInformationCircleOutline } from "react-icons/io5"
+import { IoInformationCircle } from "react-icons/io5"
+import { GoDotFill } from "react-icons/go"
 import { IoIosArrowBack } from "react-icons/io"
 import { MainLayout } from "@/components/layout"
 import Link from "next/link"
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
 
 const mockData = {
   startDate: "2021-10-01",
@@ -25,7 +36,37 @@ const electricity: NextPage = () => {
         </Link>
         <div className="flex flex-row items-center gap-2">
           <h1 className="font-bold text-2xl">Water Information</h1>
-          <IoInformationCircleOutline size={24} className="text-black" />
+          <Dialog>
+            <DialogTrigger>
+              <IoInformationCircle size={24} />
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader className="flex flex-col gap-4">
+                <DialogTitle className="flex text-left text-white">
+                  Water Information
+                </DialogTitle>
+                <DialogDescription className="flex text-white justify-start flex-col text-left gap-2">
+                  <div className="flex flex-row gap-2">
+                    <GoDotFill size={14} className="mt-1" />
+                    <p>
+                      This page shows the water usage information in this
+                      month.
+                    </p>
+                  </div>
+                  <div className="flex flex-row gap-2">
+                    <GoDotFill size={14} className="mt-1" />
+                    <p>
+                      Clicking on the{" "}
+                      <span className="text-custom-pink">
+                        "Water Meter (Image)"{" "}
+                      </span>
+                      button to see the meter image.
+                    </p>
+                  </div>
+                </DialogDescription>
+              </DialogHeader>
+            </DialogContent>
+          </Dialog>
         </div>
         <UtilityInfo
           startDate={mockData.startDate}
