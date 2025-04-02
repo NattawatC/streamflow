4f30a18158830ee451128c4b831e3402e9af9956
NattawatC <64011478@kmitl.ac.th>
2025-04-02 16:05:55 +0700

    Connect login auth with supabase

--- src/components/login/ownerForm.tsx
+++ src/components/login/ownerForm.tsx
@@ -7,7 +7,7 @@ import { z } from "zod"
 import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
 import Link from "next/link"
-import { useRouter } from "next/router"
+import { useRouter } from "next/navigation"
 import { Button } from "@/components/ui/button"
 import {
   Form,
@@ -19,30 +19,43 @@ import {
   FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
-import editTenantInfo from "@/pages/editTenantInfo"
+import { useTransition } from "react"
+import { toast } from "sonner"
+import { Loader2 } from "lucide-react"
+import { loginAction } from "@/actions/users"
 
 // needed to use, creaate new component "form" for new page
 const formSchema = z.object({
-  username: z.string().nonempty("Username is required"),
+  email: z.string().nonempty("Username is required").email(),
   password: z.string().nonempty("Password is required"),
 })
 
 export function LoginOwnerForm() {
   const router = useRouter()
+  const [isPending, startTransition] = useTransition()
+
   // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      username: "",
+      email: "",
       password: "",
     },
   })
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    console.log(values)
-    router.push("owner/home")
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    startTransition(async () => {
+      const { errorMessage } = await loginAction(values)
+
+      if (errorMessage) {
+        toast("Event has been created.")
+
+      } else {
+        router.push("/owner")
+        toast("Event has been created.")
+      }
+    })
   }
 
   return (
@@ -50,20 +63,21 @@ export function LoginOwnerForm() {
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
         <FormField
           control={form.control}
-          name="username"
+          name="email"
           render={({ field }) => (
             <FormItem>
-              <FormLabel htmlFor="username" className="text-sm">Username</FormLabel>
+              <FormLabel className="text-sm">
+                Email
+              </FormLabel>
               <FormControl>
-                <>
                   <Input
-                    id="username"
+                    id="email"
                     className="text-sm"
-                    placeholder="John"
+                    placeholder="John@gmail.com"
+                    disabled={isPending}
                     {...field}
                     icon={<BiSolidUserRectangle size={24} />}
                   />
-                </>
               </FormControl>
               <FormMessage />
             </FormItem>
@@ -74,29 +88,29 @@ export function LoginOwnerForm() {
           name="password"
           render={({ field }) => (
             <FormItem>
-              <FormLabel htmlFor="password" className="text-sm">Password</FormLabel>
+              <FormLabel htmlFor="password" className="text-sm">
+                Password
+              </FormLabel>
               <FormControl>
                 <Input
                   id="password"
                   type="password"
                   className="text-sm"
                   icon={<IoIosLock size={24} />}
+                  disabled={isPending}
                   placeholder="********"
                   {...field}
                 />
               </FormControl>
-              <FormDescription className="flex text-xs justify-center underline">
+              <FormDescription className="flex text-xs justify-center underline" >
                 <Link href={"/forgetPassword"}>Forget password?</Link>
               </FormDescription>
               <FormMessage />
             </FormItem>
           )}
         />
-        <Button
-          type="submit"
-          className="flex w-full text-base font-bold mt-8"
-        >
-          Login
+        <Button type="submit" className="flex w-full text-base font-bold mt-8" disabled={isPending}>
+          {isPending ? <Loader2 className="animate-spin"/> : "Login"}
         </Button>
       </form>
     </Form>
