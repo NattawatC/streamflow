fb1dc0706a664a6602e435b3e3235b7ace3d19c6
NattawatC <64011478@kmitl.ac.th>
2025-04-03 16:33:12 +0700

    Change select params and update function name

--- src/services/tenantService.ts
+++ src/services/tenantService.ts
@@ -1,12 +1,12 @@
 import supabase from "@/config/supabaseClient"
 
 // Function to get estates by user_id
-export const getEstates = async (userId: string | undefined) => {
+export const getEstate = async (userId: string | undefined) => {
   if (!userId) return { estates: [], error: "User ID is missing" }
 
   const { data: estates, error } = await supabase
     .from("estates")
-    .select("id")
+    .select("*")
     .eq("user_id", userId)
 
   if (error) return { estates: [], error: error.message }
@@ -17,7 +17,7 @@ export const getEstates = async (userId: string | undefined) => {
 
 // Function to get tenants based on the estates
 export const getTenants = async (userId: string | undefined) => {
-  const { estates, error: estatesError } = await getEstates(userId)
+  const { estates, error: estatesError } = await getEstate(userId)
   if (estatesError) return { tenants: [], error: estatesError }
 
   const estateIds = estates.map((estate) => estate.id)
