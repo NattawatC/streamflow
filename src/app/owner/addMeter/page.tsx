1a195e20f97dd7832f4f395677264b361bc91133
NattawatC <64011478@kmitl.ac.th>
2025-04-04 15:17:38 +0700

    Update addMeter Page

--- src/app/owner/addMeter/page.tsx
+++ src/app/owner/addMeter/page.tsx
@@ -1,13 +1,11 @@
-"use client"
-
 import { NextPage } from "next"
 import { Separator } from "@/components/ui/separator"
 import { ScrollArea } from "@/components/ui/scroll-area"
 import { MainLayout } from "@/components/layout"
 import Link from "next/link"
 import { Button } from "@/components/ui/button"
-import { AddMeterForm } from "@/components/addMeterForm"
-import { ownerData } from "@/interfaces/ownerData"
+import { AddMeterForm } from "@/components/addMeterPage/addMeterForm"
+// import { ownerData } from "@/interfaces/ownerData"
 import ElectricityMeterCard from "@/components/electricityMeterCard"
 import WaterMeterCard from "@/components/waterMeterCard"
 import {
@@ -16,8 +14,20 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion"
+import { getUser } from "@/auth/server"
+import { Electricity } from "@/components/addMeterPage/electricity"
+import {
+  getAllElectricityMeterById,
+  getUserEstateId,
+} from "@/services/ownerService"
+import { Water } from "@/components/addMeterPage/water"
+import { ElecAccordion } from "@/components/addMeterPage/elecAccordion"
+import { WaterAccordion } from "@/components/addMeterPage/waterAccordion"
 
-const addMeter: NextPage = () => {
+const addMeter: NextPage = async () => {
+  const user = await getUser()
+  const userId = user?.id
+
   return (
     <>
       <MainLayout className="flex flex-col gap-7">
@@ -30,42 +40,10 @@ const addMeter: NextPage = () => {
           <p className="font-bold text-xl">Completed</p>
           <div className="grid grid-cols-2 gap-5">
             {/* Electricity */}
-            <div className="flex bg-custom-green w-auto flex-col p-4 text-center rounded-md">
-              <div className="flex flex-col gap-2">
-                <p className="text-black font-bold text-base">
-                  Electricity Added
-                </p>
-              </div>
-              {ownerData.estate.electricityMeter.length < 10 ? (
-                <p className="text-black text-2xl font-bold">
-                  0{ownerData.estate.electricityMeter.length}{" "}
-                </p>
-              ) : (
-                <p className="text-black text-2xl font-bold">
-                  {" "}
-                  {ownerData.estate.electricityMeter.length}{" "}
-                </p>
-              )}
-            </div>
+            <Electricity userId={userId} />
 
             {/* Water*/}
-            <div className="flex bg-custom-green w-auto flex-col p-4 text-center rounded-md">
-              <div className="flex flex-col gap-2">
-                <p className="text-black font-bold text-base">
-                  Water <br /> Added
-                </p>
-              </div>
-              {ownerData.estate.electricityMeter.length < 10 ? (
-                <p className="text-black text-2xl font-bold">
-                  0{ownerData.estate.electricityMeter.length}{" "}
-                </p>
-              ) : (
-                <p className="text-black text-2xl font-bold">
-                  {" "}
-                  {ownerData.estate.electricityMeter.length}{" "}
-                </p>
-              )}
-            </div>
+            <Water userId={userId} />
           </div>
         </div>
 
@@ -73,89 +51,9 @@ const addMeter: NextPage = () => {
         <div>
           <Separator className="h-[2px] rounded-sm" />
           {/* Electricity */}
-          <Accordion type="single" collapsible>
-            <AccordionItem value="item-1">
-              <AccordionTrigger className="flex font-bold w-full justify-between">
-                Electricity Meters
-              </AccordionTrigger>
-              <AccordionContent>
-                <ScrollArea className="flex-grow-0 h-[300px] w-auto border rounded-md border-transparent">
-                  {Object.entries(
-                    ownerData.estate.electricityMeter.reduce((acc, meter) => {
-                      if (!acc[meter.floorNumber]) acc[meter.floorNumber] = []
-                      acc[meter.floorNumber].push(meter)
-                      return acc
-                    }, {} as Record<number, typeof ownerData.estate.electricityMeter>)
-                  ).map(([floor, meters]) => (
-                    <div key={floor} className="mb-5">
-                      <div className="flex flex-col gap-2">
-                        <div className="flex flex-row gap-2">
-                          <p className="whitespace-nowrap text-base font-bold">
-                            {floor} Floor
-                          </p>
-                          <div className="flex w-full items-center">
-                            <Separator className="h-[2px] rounded-sm w-full justify-center" />
-                          </div>
-                        </div>
-                        <div className="flex flex-col gap-2">
-                          {meters.map((meter, index) => (
-                            <ElectricityMeterCard
-                              key={index}
-                              floorNumber={meter.floorNumber}
-                              roomNumber={meter.roomNumber}
-                              meterNumber={meter.meterNumber}
-                            />
-                          ))}
-                        </div>
-                      </div>
-                    </div>
-                  ))}
-                </ScrollArea>
-              </AccordionContent>
-            </AccordionItem>
-          </Accordion>
+          <ElecAccordion userId={userId} />
           {/* Water */}
-          <Accordion type="single" collapsible>
-            <AccordionItem value="item-1">
-              <AccordionTrigger className="flex font-bold w-full justify-between">
-                Water Meters
-              </AccordionTrigger>
-              <AccordionContent>
-                <ScrollArea className="flex-grow-0 h-[300px] w-auto border rounded-md border-transparent">
-                  {Object.entries(
-                    ownerData.estate.electricityMeter.reduce((acc, meter) => {
-                      if (!acc[meter.floorNumber]) acc[meter.floorNumber] = []
-                      acc[meter.floorNumber].push(meter)
-                      return acc
-                    }, {} as Record<number, typeof ownerData.estate.electricityMeter>)
-                  ).map(([floor, meters]) => (
-                    <div key={floor} className="mb-5">
-                      <div className="flex flex-col gap-2">
-                        <div className="flex flex-row gap-2">
-                          <p className="whitespace-nowrap text-base font-bold">
-                            {floor} Floor
-                          </p>
-                          <div className="flex w-full items-center">
-                            <Separator className="h-[2px] rounded-sm w-full justify-center" />
-                          </div>
-                        </div>
-                        <div className="flex flex-col gap-2">
-                          {meters.map((meter, index) => (
-                            <WaterMeterCard
-                              key={index}
-                              floorNumber={meter.floorNumber}
-                              roomNumber={meter.roomNumber}
-                              meterNumber={meter.meterNumber}
-                            />
-                          ))}
-                        </div>
-                      </div>
-                    </div>
-                  ))}
-                </ScrollArea>
-              </AccordionContent>
-            </AccordionItem>
-          </Accordion>
+          <WaterAccordion userId={userId} />
         </div>
 
         {/* Meter Form Section */}
@@ -169,21 +67,23 @@ const addMeter: NextPage = () => {
                 <Separator className="h-[2px] rounded-sm w-full justify-center" />
               </div>
             </div>
-            <AddMeterForm></AddMeterForm>
+            <AddMeterForm userId={userId}/>
           </div>
         </div>
         <div className="flex flex-col gap-3">
-          <Button className="flex w-full text-base font-bold bg-custom-green text-black">
-            Update Meter
-          </Button>
-          <Link href={"/owner/home"}>
+          <Link href={"/owner"}>
+            <Button className="flex w-full text-base font-bold bg-custom-green text-black">
+              Return to Home
+            </Button>
+          </Link>
+          {/* <Link href={"/owner"}>
             <Button
               variant={"outline"}
               className="font-bold border-black outline-black bg-white text-black w-full text-base gap-2 underline"
             >
               Cancle
             </Button>
-          </Link>
+          </Link> */}
         </div>
       </MainLayout>
     </>
