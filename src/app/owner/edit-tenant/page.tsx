a28e80b4516f194ec9a9a345d7d7b815c418a30f
NattawatC <64011478@kmitl.ac.th>
2025-04-07 05:08:24 +0700

    Sending basic tenant info to editTenant component

--- src/app/owner/edit-tenant/page.tsx
+++ src/app/owner/edit-tenant/page.tsx
@@ -50,38 +50,15 @@ const editTenantInfo: NextPage = () => {
         <div className="flex flex-row justify-left gap-2">
           <h1 className="font-bold text-2xl">Edit Tenant</h1>
         </div>
-        <div className="flex flex-col gap-5 items-center bg-custom-gray-background p-4 rounded-lg">
-          <div className="flex flex-col bg-white w-full text-base p-3 gap-3 rounded-md">
-            <div className="flex flex-row gap-2">
-              <p className="whitespace-nowrap font-bold">Basic Information</p>
-              <div className="flex w-full items-center">
-                <Separator className="h-[2px] rounded-sm w-full justify-center" />
-              </div>
-            </div>
-            <div className="flex flex-row gap-4">
-              <div className="flex flex-col font-medium gap-2 text-nowrap ">
-                <p>Name:</p>
-                <p>Age:</p>
-                <p>Gender:</p>
-                <p>Phone Number:</p>
-                <p>Year Of Study:</p>
-              </div>
-              <div className="flex flex-col gap-2 text-nowrap">
-                <p>
-                  {tenant.first_name} {tenant.last_name}
-                </p>
-                <p>{tenant.age}</p>
-                <p>{tenant.gender}</p>
-                <p>{tenant.phone_number}</p>
-                <p>{tenant.year_of_study}</p>
-              </div>
-            </div>
-          </div>
-        </div>
         <div className=" flex flex-col gap-3">
-          <h1 className="font-bold text-base">Room Information</h1>
           <EditTenantForm
             id={id}
+            first_name={tenant.first_name}
+            last_name={tenant.last_name}
+            age={tenant.age}
+            gender={tenant.gender}
+            phone_number={tenant.phone_number}
+            year_of_study={tenant.year_of_study}
             building_no={tenant.building_no}
             floor_no={tenant.floor_no}
             room_no={tenant.room_no}
