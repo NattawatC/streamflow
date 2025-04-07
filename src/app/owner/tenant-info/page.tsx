e78675e1874e601b8337d684cfd297e94be7adc7
NattawatC <64011478@kmitl.ac.th>
2025-04-07 23:32:47 +0700

    Using window.location
      const [id, setId] = useState<string | null>(null)
    
      useEffect(() => {
        const params = new URLSearchParams(window.location.search)
        const id = params.get("id")
        setId(id)
      }, [])

--- src/app/owner/tenant-info/page.tsx
+++ src/app/owner/tenant-info/page.tsx
@@ -12,8 +12,13 @@ import { useEffect, useState } from "react"
 import supabase from "@/config/supabaseClient"
 
 const tenantInfo: NextPage = () => {
-  const searchParams = useSearchParams()
-  const id = searchParams.get("id")
+  const [id, setId] = useState<string | null>(null)
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search)
+    const id = params.get("id")
+    setId(id)
+  }, [])
 
   const [tenant, setTenant] = useState<any>(null)
 
