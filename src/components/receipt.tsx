3db0b1c7a9144b6ed92eaf124710f554dfc844f8
NattawatC <64011478@kmitl.ac.th>
2025-04-08 20:50:40 +0700

    Update receipt value

--- src/components/receipt.tsx
+++ src/components/receipt.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React from "react"
 import { ScrollArea } from "./ui/scroll-area"
 import { Separator } from "./ui/separator"
@@ -24,77 +26,77 @@ const Receipt: React.FunctionComponent<ReceiptProps> = ({
   roomCharge,
 }) => {
   return (
-    <>
-      <ScrollArea className="flex-grow-0 h-[300px] w-auto border rounded-md border-transparent">
-        <div className="flex flex-col gap-4">
-          <div className="flex flex-col bg-white w-full text-base p-3 gap-5 rounded-md">
-            <div className="flex flex-row gap-2">
-              <p className="whitespace-nowrap">You spend</p>
-              <div className="flex w-full items-center">
-                <Separator className="h-[2px] rounded-sm w-full justify-center" />
-              </div>
-            </div>
+    <ScrollArea className="flex-grow-0 h-[300px] w-auto border rounded-md border-transparent">
+      <span className="flex flex-col gap-4">
+        <span className="flex flex-col bg-white w-full text-base p-3 gap-5 rounded-md">
+          <span className="flex flex-row gap-2">
+            <span className="whitespace-nowrap">You spend</span>
+            <span className="flex w-full items-center">
+            <Separator className="h-[2px] rounded-sm w-full justify-center" />
+          </span>
+          </span>
 
-            <div className="flex flex-row justify-between items-center font-medium">
-              <p>
-                {startDate} - {endDate}
-              </p>
-              <p>Date</p>
-            </div>
-            {/* Electricity */}
-            <div className="flex flex-row gap-2">
-              <p className="whitespace-nowrap">Electricity</p>
-              <div className="flex w-full items-center">
-                <Separator className="h-[2px] rounded-sm w-full justify-center" />
-              </div>
-            </div>
-            <div className="flex flex-row justify-between items-center font-medium">
-              <li>{eUsed}</li>
-              <p>Usage (kWh)</p>
-            </div>
-            <div className="flex flex-row justify-between items-center font-medium">
-              <li>{eCost}</li>
-              <p>Cost (Baht)</p>
-            </div>
-            {/* Water */}
-            <div className="flex flex-row gap-2">
-              <p className="whitespace-nowrap">Water</p>
-              <div className="flex w-full items-center">
-                <Separator className="h-[2px] rounded-sm w-full justify-center" />
-              </div>
-            </div>
-            <div className="flex flex-row justify-between items-center font-medium">
-              <li>{wUsed}</li>
-              <p>Usage (m³)</p>
-            </div>
-            <div className="flex flex-row justify-between items-center font-medium">
-              <li>{wCost}</li>
-              <p>Cost (Baht)</p>
-            </div>
-          </div>
+          <span className="flex flex-row justify-between items-center font-medium">
+            <span>
+              {startDate} - {endDate}
+            </span>
+            <span>Date</span>
+          </span>
+          {/* Electricity */}
+          <span className="flex flex-row gap-2">
+            <span className="whitespace-nowrap">Electricity</span>
+            <span className="flex w-full items-center">
+            <Separator className="h-[2px] rounded-sm w-full justify-center" />
+          </span>
+          </span>
+          <span className="flex flex-row justify-between items-center font-medium">
+            <li>{eUsed}</li>
+            <span>Usage (kWh)</span>
+          </span>
+          <span className="flex flex-row justify-between items-center font-medium">
+            <li>{eCost}</li>
+            <span>Cost (Baht)</span>
+          </span>
+          {/* Water */}
+          <span className="flex flex-row gap-2">
+            <span className="whitespace-nowrap">Water</span>
+            <span className="flex w-full items-center">
+            <Separator className="h-[2px] rounded-sm w-full justify-center" />
+          </span>
+          </span>
+          <span className="flex flex-row justify-between items-center font-medium">
+            <li>{wUsed}</li>
+            <span>Usage (m³)</span>
+          </span>
+          <span className="flex flex-row justify-between items-center font-medium">
+            <li>{wCost}</li>
+            <span>Cost (Baht)</span>
+          </span>
+        </span>
 
-          <Separator className="h-[2px] rounded-sm w-full justify-center" />
+        <Separator className="h-[2px] rounded-sm w-full justify-center" />
 
-          {/* Accomodation Fee */}
-          <div className="flex flex-col bg-white w-full text-base p-3 gap-5 rounded-md">
-            <div className="flex flex-row gap-2">
-              <p className="whitespace-nowrap font-normal">Accomodation Fee</p>
-              <div className="flex w-full items-center">
-                <Separator className="h-[2px] rounded-sm w-full justify-center" />
-              </div>
-            </div>
-            <div className="flex flex-row justify-between items-center font-medium">
-              <li>{furniture}</li>
-              <p>Furniture</p>
-            </div>
-            <div className="flex flex-row justify-between items-center font-medium">
-              <li>{roomCharge}</li>
-              <p>Room Charge</p>
-            </div>
-          </div>
-        </div>
-      </ScrollArea>
-    </>
+        {/* Accomodation Fee */}
+        <span className="flex flex-col bg-white w-full text-base p-3 gap-5 rounded-md">
+          <span className="flex flex-row gap-2">
+            <span className="whitespace-nowrap font-normal">
+              Accomodation Fee
+            </span>
+            <span className="flex w-full items-center">
+            <Separator className="h-[2px] rounded-sm w-full justify-center" />
+          </span>
+          </span>
+          <span className="flex flex-row justify-between items-center font-medium">
+            <li>{furniture}</li>
+            <span>Furniture</span>
+          </span>
+          <span className="flex flex-row justify-between items-center font-medium">
+            <li>{roomCharge}</li>
+            <span>Room Charge</span>
+          </span>
+        </span>
+      </span>
+    </ScrollArea>
   )
 }
 
